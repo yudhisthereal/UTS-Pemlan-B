39d8e7e8a7017af84a2f95ba86c0154fbd9b69dd
yudhisthereal <63510362+yudhisthereal@users.noreply.github.com>
2023-10-19 15:47:16 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ from functions.add import add_housing
 from functions.search import *
 from functions.delete import delete_housing
 
+df = pd.read_csv(FILE_PATH)
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
 
 class Choice(Enum):
+    HELP = 0
     SHOW = 1
     SEARCH = 2
     ADD = 3
@@ -35,6 +37,29 @@ def int_input(message):
     return str(result)
 
 
+def show_help():
+    print("""1. Show 
+          This function is used to display housing in order. sorting can 
+          be done by id, name, host name, price, and availability 
+          for a year.""")
+    print("""2. Search
+          This function is used to search for lodging based on 
+          id, name, host, neighborhood, price, and minimum nights. 
+          It will display all housing that matches the search.""")
+    print("""3. Add 
+          This function is used to add new housing data to the database. 
+          It will ask for important data that must be added, 
+          such as housing name, host id, host name, etc.""")
+    print("""4. Update 
+          This function is used to update housing data in the database. 
+          It will ask if you want to update the data. 
+          Press 'y' for yes and 'n' for no.""")
+    print("""5. Delete 
+          This function is used to delete housing data in the database. 
+          It will ask which housing ID that want to be deleted.""")
+
+
+
 def float_input(message):
     input_is_valid = False
     result = ''
@@ -53,6 +78,7 @@ print("="*5 + " Welcome to New York Housing Admin" + "="*5)
 while True:
     print('')
     print("What do you wish to do?")
+    print("0. Show help")
     print("1. Show housing data")
     print("2. Search for housing data")
     print("3. Add new housing data")
@@ -63,7 +89,10 @@ while True:
     choice = int_input('enter your choice : ')
     choice = int(choice)
 
-    if (choice == Choice.SHOW.value):
+    if (choice == Choice.HELP.value):
+        show_help()
+    
+    elif (choice == Choice.SHOW.value):
         columns = ['id', 'name', 'host_name', 'price', 'availability_365']
         print('Sort results by')
         for i, column in enumerate(columns):
@@ -94,7 +123,7 @@ while True:
                     print('Input must one of (y/n)')
                 
 
-        show(sorted_column, ascending)
+        show(df, sorted_column, ascending)
 
     elif (choice == Choice.SEARCH.value):
         column_names = ('id', 'name', 'host_name', 'neighbourhood', 'price', 'minimum_nights')
@@ -118,7 +147,7 @@ while True:
             query = input('Enter query : ')
         else:
             query = int_input('Enter query : ')
-        results = search(query, column_names[search_by])
+        results = search(df, query, column_names[search_by])
 
         results = results.head(25) if search_by != 0 else results
 
@@ -144,9 +173,10 @@ while True:
         room_type = input('enter room type : ').strip()
         price = input('enter price : ').strip()
         minimum_nights = input('enter minimum nights : ').strip()
+        availability = input('enter availability : ').strip()
 
-        add_housing(name, host_id, host_name, neighbourhood_group,
-                    neighbourhood, latitude, longitude, room_type, price, minimum_nights)
+        add_housing(df, name, host_id, host_name, neighbourhood_group,
+                    neighbourhood, latitude, longitude, room_type, price, minimum_nights, availability)
         
         print(f'the new housing, "{name}" and its information were added successfully')
 
@@ -200,7 +230,7 @@ while True:
     elif (choice == Choice.DELETE.value):
         housing_id = int(input('Enter housing ID : '))
         if (id_exists(housing_id)):
-            delete_housing(housing_id)
+            delete_housing(df, housing_id)
             print('Deletion successful')
         else:
             print('id not found')
